4b420a70065d504569c5704f3721d0629a1660fe
Pallabi Mukherjee <162665463+its-Pallabi@users.noreply.github.com>
2024-07-19 13:26:16 +0530

    Update anomaly_detection_dataset.py

--- momentfm/data/anomaly_detection_dataset.py
+++ momentfm/data/anomaly_detection_dataset.py
@@ -22,9 +22,9 @@ class AnomalyDetectionDataset:
         """
 
         self.full_file_path_and_name = (
-            "moment/data/198_UCR_Anomaly_tiltAPB2_50000_124159_124985.out"
+            "moment/data/1_MBA_ECG_dataset_50000_data.out"
         )
-        self.series = "198_UCR_Anomaly_tiltAPB2_50000_124159_124985.out"
+        self.series = "1_MBA_ECG_dataset_50000_data.out"
         self.data_split = data_split
         self.data_stride_len = data_stride_len
         self.random_seed = random_seed
